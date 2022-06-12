07a7f3e4edd24351bd54191f49bd6d381d2ca797
AswinRaaj R I <aswinraaj26@gmail.com>
2022-06-12 23:17:33 +0530

    Change password and reset password views

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -16,6 +16,8 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
