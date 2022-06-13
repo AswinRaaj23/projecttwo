7213daaa39cf2d7c7bae7d43bd902fc499dcc2b5
AswinRaaj <aswin@testpress.in>
2022-06-13 10:45:13 +0530

    Create User Registration

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     #path('login/', views.user_login, name='login'),
     # path('login/', auth_views.LoginView.as_view(), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
-
+    
     # path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
@@ -16,6 +15,7 @@ urlpatterns = [
     # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
+    path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
