8e45a558a396bf0a60342b4a110412c47773fcb9
AswinRaaj <aswin@testpress.in>
2022-06-15 13:55:50 +0530

    Add ajax pagination to list views

---
+++ bookmarks/common/decorators.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponseBadRequest
+from .ajax import is_ajax
+
+def ajax_required(f):
+    def wrap(request, *args, **kwargs):
+        if not is_ajax(request):
+            return HttpResponseBadRequest()
+        return f(request, *args, **kwargs)
+    wrap.__doc__=f.__doc__
+    wrap.__name__=f.__name__
+    return wrap
